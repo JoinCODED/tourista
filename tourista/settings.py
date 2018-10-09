ba066a0acedb68708b2cbc1eff82f3fafa57def9
Octowl <alsaff1987@gmail.com>
2018-10-09 10:28:31 +0300

    Reinclude static files

--- tourista/settings.py
+++ tourista/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = ...
-# STATIC_ROOT = ...
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
